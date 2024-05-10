935213e80017158420e77561149b3687258ac796
guzmanor4747 <300064747@fvtc.edu>
2024-05-10 06:48:23 -0500

    done

--- api-routes/api-routes.js
+++ api-routes/api-routes.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 const { getCollection, ObjectId } = require('../todo-db')
 
-
 // GET /api/todos
-router.get('/', async(_, response) => {
+router.get('/', async (_, response) => {
     const collection = await getCollection('todo-api', 'todos')
     const todos = await collection.find({}).toArray()
-    response.json(todos);
+    // Map to rename _id to id to use it on the put 
+    const updatedId = todos.map(todo => {
+        const { _id, ...rest } = todo
+        return { id: _id, ...rest }
+    });
+
+    response.json(updatedId);
 });
 
 // POST /api/todos
@@ -21,17 +26,17 @@ router.post('/', async (request, response) => {
 	response.json(result)
 })
 
-//router.put('/:id', async (request, response) => {
 // PUT /api/todos/:id
-router.put('/:id', async(request, response) => {
-	const { id } = request.params
-    const collection = await getCollection('todo-api', 'todos')
-    const todo = await collection.findOne({ _id: new ObjectId(id) })
-const complete = !todo.complete
-const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { complete } })
-response.json(result)
-
-
+router.put('/:id', async (request, response) => {
+   
+    
+        const { id } = request.params;
+        const collection = await getCollection('todo-api', 'todos');
+        const todo = await collection.findOne({ _id: new ObjectId(id) });
+        const complete = !todo.complete;
+        const result = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { complete } });
+       
+        response.json(result); 
 }); 
 
 
